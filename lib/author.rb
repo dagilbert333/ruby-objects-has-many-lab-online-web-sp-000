8b1af6dffeb9d40548724e834bb2198018f469f9
David Gilbert <dagilbert333@gmail.com>
2019-03-03 08:08:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author 
   attr_accessor :name
-  @@total_posts
+  @@total_posts = 0
   
   def initialize(name)
     @name = name
@@ -14,12 +14,14 @@ class Author
   def add_post(post_name)
     @posts << post_name
     post_name.author = self
+    @@total_posts += 1
   end
   
   def add_post_by_title(post_title)
     new_post = Post.new(post_title)
     @posts << new_post
     new_post.author = self
+    @@total_posts += 1
   end
   
   def post_count
